05b222282dd654cc7e3379e6722de18fda629dd9
thomasbryk <thomasbryk@cmail.carleton.ca>
2020-02-14 14:20:07 -0500

    Fixed tests

--- src/test/java/TestingWebApplicationTest.java
+++ src/test/java/TestingWebApplicationTest.java
@@ -23,12 +23,19 @@ public class TestingWebApplicationTest {
 
     @Test
     public void createAddressBook() throws Exception {
-        this.mockMvc.perform(post("/addressbook")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(post("/rest_addAddressbook")).andDo(print()).andExpect(status().isOk());
     }
 
     @Test
     public void getAddressBook() throws Exception {
         this.mockMvc.perform(post("/rest_addAddressbook"));
-        this.mockMvc.perform(get("/rest_getAddressBook").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("id", "1")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("AddressBook id=1")));
+        this.mockMvc.perform(get("/rest_getAddressBook").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("id", "1")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void createBuddies() throws Exception {
+        this.mockMvc.perform(post("/rest_addAddressbook"));
+        this.mockMvc.perform(get("/rest_getAddressBook").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("id", "1")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(post("/rest_addBuddy").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("id", "1").param("name", "Thomas").param("number", "123456789")).andDo(print()).andExpect(status().isOk());
     }
 }
